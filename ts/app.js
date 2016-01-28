0c94462dd405acdc1e0fa1ee40c4803cd1304e66
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-01-28 23:54:22 +0100

    Fixed ts I think? Included more files and changed the demo.

---
+++ ts/app.js
@@ -0,0 +1,21 @@
+///<reference path='./phaser.d.ts'/>""
+var SimpleGame = (function () {
+    function SimpleGame() {
+        var w = window.screen.width;
+        var h = window.screen.height;
+        this.game = new Phaser.Game(w, h, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
+    }
+    SimpleGame.prototype.preload = function () {
+        this.game.load.image('logo', 'test.jpeg');
+    };
+    SimpleGame.prototype.create = function () {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    };
+    SimpleGame.prototype.update = function () {
+    };
+    return SimpleGame;
+})();
+window.onload = function () {
+    var game = new SimpleGame();
+};
--- ts/app.ts
+++ ts/app.ts
@@ -4,16 +4,21 @@ class SimpleGame {
     constructor() {
         var w = window.screen.width
         var h = window.screen.height
-        this.game = new Phaser.Game(w, h, Phaser.AUTO, 'content', { preload: this.preload, update: this.update });
+        this.game = new Phaser.Game(w, h, Phaser.AUTO, 'content', { preload: this.preload, create:this.create, update: this.update });
     }
     game: Phaser.Game;
-
+    
     preload() {
+        this.game.load.image('logo', 'test.jpeg');
+    }
 
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
     }
 
     update() {
-
+    
     }
 }
 
